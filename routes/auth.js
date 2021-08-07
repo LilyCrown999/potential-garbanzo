0d69606dde76a818ba792e3aa8cbcd69c12d0d62
LilyCrown999 <adexadexdede@gmail.com>
2021-08-07 13:33:35 +0100

    Working checkpoint -fixed Dashboard

--- routes/auth.js
+++ routes/auth.js
@@ -18,5 +18,11 @@ router.get(
     res.redirect('/dashboard')
 })
 
+//@desc     Logout user
+//@route    /auth/logout
+router.get('/logout', (req,res) => {
+    req.logout()
+    res.redirect('/')
+})
 
 module.exports = router; 
